881f5d5932f067cc0b56c7d6fa4edb7364cc6b70
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-05 13:46:10 +0200

    kaffe opgave done

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    class FlatWhite: Kaffe,Imælk
+    {
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public virtual string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
